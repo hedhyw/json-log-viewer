ad60234b24d0a8cd0af45b6517910565cd1341ae
hedhyw <hedhyw@yahoo.com>
2023-07-25 09:57:35 +0300

    fix: long lines

--- internal/pkg/source/source.go
+++ internal/pkg/source/source.go
@@ -4,9 +4,16 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+)
+
+const (
+	maxLineSize = 8 * 1024 * 1024
+
+	logEntriesEstimateNumber = 256
 )
 
 // LoadLogsFromFile loads json log entries from file.
@@ -19,22 +26,33 @@ func LoadLogsFromFile(path string) func() tea.Msg {
 
 		defer file.Close()
 
-		scanner := bufio.NewScanner(file)
-
-		logEntries := make(LogEntries, 0, 256)
-
-		for scanner.Scan() {
-			line := scanner.Bytes()
-
-			if len(bytes.TrimSpace(line)) > 0 {
-				logEntries = append(logEntries, ParseLogEntry(line))
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			return fmt.Errorf("scanning: %w", err)
+		logEntries, err := parseLogEntriesFromReader(file)
+		if err != nil {
+			return fmt.Errorf("parsing from reader: %w", err)
 		}
 
 		return logEntries.Reverse()
 	}
+}
+
+func parseLogEntriesFromReader(reader io.Reader) (LogEntries, error) {
+	bufReader := bufio.NewReaderSize(reader, maxLineSize)
+	logEntries := make(LogEntries, 0, logEntriesEstimateNumber)
+
+	for {
+		line, _, err := bufReader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, fmt.Errorf("reading line: %w", err)
+		}
+
+		if len(bytes.TrimSpace(line)) > 0 {
+			logEntries = append(logEntries, ParseLogEntry(line))
+		}
+	}
+
+	return logEntries, nil
 }
