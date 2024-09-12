hedhyw/json-log-viewer

commit ec18c3c63dd04f6d1283f877a4c79a633bd6f8cb

Improvement based on PR comments.

internal/pkg/source/source.go
@@ -15,13 +15,20 @@ const (
 )
 
 type Source struct {
-	Seeker         *os.File
-	reader         *bufio.Reader
-	file           *os.File
-	offset         int64
+	// Seeker is used to do random access reads from the file.
+	Seeker *os.File
+	// Reader is used to read the file sequentially.
+	reader *bufio.Reader
+	// The log file we are reading from, or a temp file we are writing to (depending on if created with File or Reader func).
+	file *os.File
+	// offset is the next offset a long entry will be read from.
+	offset int64
+	// prevFollowSize is the size of the file the last time we checked
 	prevFollowSize int64
-	name           string
-	maxSize        int64
+	// name is the name of the file we are reading.
+	name string
+	// maxSize is the maximum size of the file we will read.
+	maxSize int64
 }
 
 func (is *Source) Close() (err error) {
@@ -33,6 +40,7 @@ func (is *Source) Close() (err error) {
 	return err
 }
 
+// File creates a new Source for reading log entries from a file.
 func File(name string, cfg *config.Config) (*Source, error) {
 	var err error
 	is := &Source{
@@ -55,6 +63,8 @@ func File(name string, cfg *config.Config) (*Source, error) {
 	return is, nil
 }
 
+// Reader creates a new Source for reading log entries from an io.Reader.  This will write the input to a temp file.
+// which will be used to seek against.
 func Reader(input io.Reader, cfg *config.Config) (*Source, error) {
 	var err error
 	is := &Source{
@@ -67,8 +77,10 @@ func Reader(input io.Reader, cfg *config.Config) (*Source, error) {
 		return nil, err
 	}
 
+	// The io.TeeReader will write the input to the is.file as it is read.
 	reader := io.TeeReader(input, is.file)
 
+	// We can now seek against the data that is read in the input io.Reader.
 	is.Seeker, err = os.Open(is.file.Name())
 	if err != nil {
 		_ = is.file.Close()

internal/pkg/source/sreamer.go
@@ -8,51 +8,15 @@ import (
 	"time"
 )
 
+const InitialLogSize int = 1000
+
 func (is *Source) StartStreaming(ctx context.Context, send func(msg LazyLogEntries, err error)) {
-	mu := sync.Mutex{}
-	logEntries := make([]LazyLogEntry, 0, 1000)
+	logEntriesLock := sync.Mutex{}
+	logEntries := make([]LazyLogEntry, 0, InitialLogSize)
 	eofEvent := make(chan struct{}, 1)
+
 	// Load log entries async..
-	go func() {
-		defer func() {
-			eofEvent <- struct{}{}
-		}()
-
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
-
-			entry, err := is.ReadLogEntry()
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					if !is.CanFollow() {
-						return
-					}
-
-					// wait for new log entries to be written to the file,
-					// and try again.
-					ticker := time.NewTicker(200 * time.Millisecond)
-					select {
-					case <-ctx.Done():
-						ticker.Stop()
-						return
-					case <-ticker.C:
-						ticker.Stop()
-					}
-
-					continue
-				}
-				send(LazyLogEntries{}, err)
-				return
-			}
-			mu.Lock()
-			logEntries = append(logEntries, entry)
-			mu.Unlock()
-		}
-	}()
+	go is.readLogEntries(ctx, send, &logEntriesLock, logEntries, eofEvent)
 
 	// periodically send new log entries to the program.
 	go func() {
@@ -63,9 +27,9 @@ func (is *Source) StartStreaming(ctx context.Context, send func(msg LazyLogEntri
 		sendUpdates := func() {
 			// Only send log update the program state every ticker seconds,
 			// to avoid stressing the main loop.
-			mu.Lock()
+			logEntriesLock.Lock()
 			entries := logEntries
-			mu.Unlock()
+			logEntriesLock.Unlock()
 
 			nextLen := len(entries)
 			if lastLen != nextLen {
@@ -91,4 +55,46 @@ func (is *Source) StartStreaming(ctx context.Context, send func(msg LazyLogEntri
 			}
 		}
 	}()
+}
+
+func (is *Source) readLogEntries(ctx context.Context, send func(msg LazyLogEntries, err error), logEntriesLock *sync.Mutex, logEntries []LazyLogEntry, eofEvent chan struct{}) {
+	defer func() {
+		eofEvent <- struct{}{}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
+		entry, err := is.ReadLogEntry()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+
+				if !is.CanFollow() {
+					return
+				}
+
+				// wait for new log entries to be written to the file,
+				// and try again.
+				ticker := time.NewTicker(200 * time.Millisecond)
+				select {
+				case <-ctx.Done():
+					ticker.Stop()
+					return
+				case <-ticker.C:
+					ticker.Stop()
+				}
+
+				continue
+			}
+			send(LazyLogEntries{}, err)
+			return
+		}
+		logEntriesLock.Lock()
+		logEntries = append(logEntries, entry)
+		logEntriesLock.Unlock()
+	}
 }

internal/app/lazytable.go
@@ -68,45 +68,59 @@ func (m lazyTableModel) Update(msg tea.Msg) (lazyTableModel, tea.Cmd) {
 }
 
 func (m lazyTableModel) handleKey(msg tea.KeyMsg, render bool) (lazyTableModel, bool) {
+
+	// toggle the reverse display of items.
 	if key.Matches(msg, m.Application.keys.Reverse) {
 		m.reverse = !m.reverse
 		render = true
 	}
 
+	// this function increases the viewport offset by 1 if possible.  (scrolls down)
 	increaseOffset := func() {
-		maxO := max(m.entries.Len()-m.table.Height(), 0)
-		o := min(m.offset+1, maxO)
+		maxOffset := max(m.entries.Len()-m.table.Height(), 0)
+		o := min(m.offset+1, maxOffset)
 		if o != m.offset {
 			m.offset = o
 			render = true
 		} else {
+			// we were at the last item, so we should follow the log
 			m.follow = true
 		}
 	}
+
+	// this function decreases the viewport offset by 1 if possible.  (scrolls up)
 	decreaseOffset := func() {
-		o := max(m.offset-1, 0)
-		if o != m.offset {
-			m.offset = o
+		offset := max(m.offset-1, 0)
+		if offset != m.offset {
+			m.offset = offset
 			render = true
+		} else {
+			// we were at the first item, so we should follow the log
+			m.follow = true
 		}
 	}
+
+	// if the table is being displayed in reverse order, we need to swap the increase and decrease functions
+	// since the last item is at the top of the table instead of the bottom.
 	if m.reverse {
 		increaseOffset, decreaseOffset = decreaseOffset, increaseOffset
 	}
+
 	if key.Matches(msg, m.Application.keys.Down) {
 		m.follow = false
 		if m.table.Cursor()+1 == m.table.Height() {
-			increaseOffset()
+			increaseOffset() // move the viewport
 		}
 	}
 	if key.Matches(msg, m.Application.keys.Up) {
 		m.follow = false
 		if m.table.Cursor() == 0 {
-			decreaseOffset()
+			decreaseOffset() // move the viewport
 		}
 	}
 	if key.Matches(msg, m.Application.keys.GotoTop) {
 		if m.reverse {
+			// when follow is enabled, rendering will handle setting the offset to the correct value
 			m.follow = true
 		} else {
 			m.follow = false
@@ -119,6 +133,7 @@ func (m lazyTableModel) handleKey(msg tea.KeyMsg, render bool) (lazyTableModel, 
 			m.follow = false
 			m.offset = 0
 		} else {
+			// when follow is enabled, rendering will handle setting the offset to the correct value
 			m.follow = true
 		}
 		render = true

commit 0ac6191fa53db51b4df656f49dc9e4db938bd1c6

fix: log entries were not being loaded
Broke in ec18c3c63dd04f6d1283f877a4c79a633bd6f8cb

Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

internal/pkg/source/source.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
-	"github.com/hedhyw/semerr/pkg/v1/semerr"
 	"io"
 	"os"
+
+	"github.com/hedhyw/semerr/pkg/v1/semerr"
 
 	"github.com/hedhyw/json-log-viewer/internal/pkg/config"
 )

internal/pkg/source/sreamer.go
@@ -16,7 +16,7 @@ func (is *Source) StartStreaming(ctx context.Context, send func(msg LazyLogEntri
 	eofEvent := make(chan struct{}, 1)
 
 	// Load log entries async..
-	go is.readLogEntries(ctx, send, &logEntriesLock, logEntries, eofEvent)
+	go is.readLogEntries(ctx, send, &logEntriesLock, &logEntries, eofEvent)
 
 	// periodically send new log entries to the program.
 	go func() {
@@ -57,7 +57,7 @@ func (is *Source) StartStreaming(ctx context.Context, send func(msg LazyLogEntri
 	}()
 }
 
-func (is *Source) readLogEntries(ctx context.Context, send func(msg LazyLogEntries, err error), logEntriesLock *sync.Mutex, logEntries []LazyLogEntry, eofEvent chan struct{}) {
+func (is *Source) readLogEntries(ctx context.Context, send func(msg LazyLogEntries, err error), logEntriesLock *sync.Mutex, logEntries *[]LazyLogEntry, eofEvent chan struct{}) {
 	defer func() {
 		eofEvent <- struct{}{}
 	}()
@@ -93,7 +93,7 @@ func (is *Source) readLogEntries(ctx context.Context, send func(msg LazyLogEntri
 			return
 		}
 		logEntriesLock.Lock()
-		logEntries = append(logEntries, entry)
+		*logEntries = append(*logEntries, entry)
 		logEntriesLock.Unlock()
 	}
 }